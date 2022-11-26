d31063db112515c20794714da91083595fb328a9
Ravan Hashimov <ravanhashimov@gmail.com>
2022-11-26 17:04:20 +0300

    Add Database

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp;
+using PokemonReviewApp.Data;
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -5,10 +9,29 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
+        builder.Services.AddTransient<Seed>();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
 
         var app = builder.Build();
+
+        if (args.Length == 1 && args[0].ToLower() == "seeddata")
+            SeedData(app);
+
+        void SeedData(IHost app)
+        {
+            var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+            using (var scope = scopedFactory.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetService<Seed>();
+                service.SeedDataContext();
+            }
+        }
 
         if (app.Environment.IsDevelopment())
         {
